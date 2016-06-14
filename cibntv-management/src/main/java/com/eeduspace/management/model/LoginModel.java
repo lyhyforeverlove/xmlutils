698a8765d20f010660d49a8634d6999ba1a74bdf
zhuchaowei <zhuchaowei@e-eduspace.com>
2016-06-14 17:25:50 +0800

    model提交

---
+++ cibntv-management/src/main/java/com/eeduspace/management/model/LoginModel.java
@@ -0,0 +1,18 @@
+package com.eeduspace.management.model;
+
+public class LoginModel {
+	private String user;
+	private String password;
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
