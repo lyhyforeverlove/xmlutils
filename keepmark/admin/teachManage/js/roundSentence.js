20cb3de43fa3ffc614ae61d67a77f3cf3a2fae7f
zhuchaowei <zhuchaowei@e-eduspace.com>
2016-09-18 18:15:12 +0800

    判卷页面

--- keepmark/admin/teachManage/js/roundSentence.js
+++ keepmark/admin/teachManage/js/roundSentence.js
@@ -25,9 +25,7 @@ app.controller('RoundController', function($scope, $http, $controller, $resource
 			})
 			.success(function(data) {
 				$scope.results = data.result;
-
 				$scope.totalPage = data.result.totalPage;
-
 				callback && callback(data.result);
 			}).error(function(data) {
 
@@ -58,7 +56,7 @@ app.controller('SecondRoundController', function($scope, $http, $controller, $re
 	$scope.formData.gradeCode = 33;
 	//根据学年、类型、学科、教材查询一轮判列表
 	$scope.getList = function(page, size, callback) {
-		$http.post($scope.app.host + 'teacher/diagnosis/getTwoRound?requestId=test123456', {
+		$http.post($scope.app.testhost + 'teacher/diagnosis/getTwoRound?requestId=test123456', {
 				"subjectCode": $scope.formData.subjectCode,
 				"bookVersionCode": $scope.formData.bookVersionCode,
 				"currentPage": page,
@@ -70,14 +68,12 @@ app.controller('SecondRoundController', function($scope, $http, $controller, $re
 				"pageSize": size
 			})
 			.success(function(data) {
-				console.log(data);
 				$scope.results = data.result;
 
 				$scope.totalPage = data.result.totalPage;
 
 				callback && callback(data.result);
 			}).error(function(data) {
-				console.log(data);
 
 			});
 	};
@@ -92,47 +88,42 @@ app.controller('SecondRoundController', function($scope, $http, $controller, $re
 
 	};
 
-	//	$scope.sayHello = function() {
-	//		$scope.$emit('$fromSubControllerClick','hello');
-	//	};
 
 });
 
 /**判卷详情*/
 app.controller('MarkExamController', function($scope, $http, $controller, $resource, $stateParams, $modal, $state) {
-
 	$controller('ParentGetDataCtrl', {
 		$scope: $scope
 	}); //继承
-
+	//
 	var markPaperRequestJson = null;
 	var postObj = {}; //判卷提交试题
 	var markModel = {}; //单题信息
 	var productionModel = {};
+	var subjectivityScore = 0; //主观得分
+	var impersonalityScore = 0; //客观得分
 	markModel.productionList = [];
 	postObj.list = [];
 	// 获取上个界面传递的数据，并进行解析  
 	if($stateParams.jsonString != '') {
 		markPaperRequestJson = angular.fromJson($stateParams.jsonString);
 	}
-	console.log(markPaperRequestJson);
 	$scope.paperDetail = markPaperRequestJson;
-	// $scope.persons = markPaperFactory.getter();
-	//	$scope.$on('$fromSubControllerClick', function(e,data){
-	//	console.log(data); // hello
+	impersonalityScore = markPaperRequestJson.impersonalityScore;
 	//获取学生答题详情
-	$http.post($scope.app.testhost + 'student/diagnosis/getUserAnswer?requestId=test123456', {
-			"studentCode": "1111",
-			"singleDiagnosisRecordCode": "5948BBC0862844B3808D668FD0E4E51F"
-		})
-		.success(function(data) {
-			console.log(data);
-			$scope.results = data.result;
-			//$state.go("app.teachManage.examDetail");
-		}).error(function(data) {
-			console.log(data);
+	$scope.load = function() {
+		$http.post($scope.app.host + 'student/diagnosis/getUserAnswer?requestId=test123456', {
+				"studentCode": markPaperRequestJson.studentCode,
+				"singleDiagnosisRecordCode": markPaperRequestJson.eduSingleDiagnosisRecordCode
+			})
+			.success(function(data) {
+				$scope.results = data.result;
+				//$state.go("app.teachManage.examDetail");
+			}).error(function(data) {
 
-		});
+			});
+	}
 
 	$scope.addErrorKnown = function() {
 			var addHtml = $('.survey-knowledge').children('div').children('a');
@@ -146,7 +137,6 @@ app.controller('MarkExamController', function($scope, $http, $controller, $resou
 		//组装判卷信息
 	$scope.submitQuestion = function(data) {
 
-		console.log(data);
 		data.productionList = [];
 		//根据组装数据填充 产生式信息
 		//		productionModel.productionName = '产生式名称';
@@ -154,33 +144,53 @@ app.controller('MarkExamController', function($scope, $http, $controller, $resou
 		//		productionModel.knowledgeName = '知识点名称';
 		//		productionModel.knowledgeCode = '知识点code';
 		//		markModel.productionList.push(productionModel);
-		markModel.answerRecordCode=data.eduSingleDiagnosisRecordCode;
-		markModel.questionScore=data.questionScore;
-		markModel.score=data.paperScore+data.questionScore;
-		markModel.urfaceScore=data.paperScore;
-		markModel.errorProductions='[{"productionName":"productionName0","productionCode":"productionCode0","knowledgeName":"knowledgeName0","knowledgeCode":"knowledgeCode0"}]';
+		markModel.answerRecordCode = data.eduSingleDiagnosisRecordCode;
+		markModel.questionScore = data.questionScore;
+		markModel.score = data.questionScore;
+		markModel.answerRecordCode = data.code;
+		markModel.urfaceScore = data.paperScore;
+
+		markModel.errorProductions = '[{"productionName":"productionName0","productionCode":"productionCode0","knowledgeName":"knowledgeName0","knowledgeCode":"knowledgeCode0"}]';
 		//markModel.questionType='';//试题类型
 		//markModel.sentenceResult='';//判卷结果
 		postObj.list.push(markModel);
-		console.log(postObj);
+		subjectivityScore = parseFloat(subjectivityScore) + parseFloat(data.paperScore) + parseFloat(data.questionScore);
+		console.log(subjectivityScore);
+		$("#" + data.code).attr('disabled', true);
 	}
 
 	//提交最终判卷结果
 	$scope.submitPaper = function() {
-		$http.post("http://192.168.1.156:8090/" + 'teacher/diagnosis/mark?requestId=test123456', {
-				"diagnosticRecordsCode": "1111",
-				"markType": 1, //一判 二判 复审
+		console.log($scope.check());
+		console.log(parseFloat(subjectivityScore) + parseFloat(impersonalityScore));
+		console.log(subjectivityScore);
+
+		$http.post($scope.app.host + 'teacher/diagnosis/mark?requestId=test123456', {
+				"diagnosticRecordsCode": markPaperRequestJson.eduSingleDiagnosisRecordCode,
+				"markType":markPaperRequestJson.markRound , //一判 二判 复审
+				"teacherCode": "111111",//获取登录教师的code
+				"teacherName": "教师名称",//获取登录教师的名称
+				"subjectivityScore": subjectivityScore, //主观题得分
+				"singleScore": subjectivityScore + parseFloat(impersonalityScore), //单科总分
 				"markModels": postObj.list
 			})
 			.success(function(data) {
-				console.log(data);
-				//$state.go("app.teachManage.examDetail");
+				//根据判卷轮次跳转不同的页面
 			}).error(function(data) {
-				console.log(data);
 
 			});
 
 	}
+
+	$scope.check = function() {
+		$(".col-sm-3 button").each(function(index, value) {
+			if(typeof($(this).attr('disabled')) == "undefined") {
+				alert("还有题未进行提交！！请检查！！！");
+				return false;
+			}
+		});
+	}
+
 });
 
 function removehtml(id) {
