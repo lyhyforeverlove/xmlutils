ec367892e2722afaf4e72d4725694c8d8362bda7
dingran <dingran@e-eduspace.com>
2016-09-22 17:12:41 +0800

    修改短板选择诊断用卷

--- keepmark/admin/teachResearchManage/js/controllerHXY.js
+++ keepmark/admin/teachResearchManage/js/controllerHXY.js
@@ -309,12 +309,27 @@ app.controller('ModalShortSlabCtrlDR',function($scope,$http,$controller, $modalI
 
 });
 /*学生分类=》短板确认*/
-app.controller('SBConfrimCtrl', function($scope, $controller,CalcService, $http) {
+app.controller('SBConfrimCtrl', function($scope, $http, $resource, $stateParams, $modal,$log, $state,$controller,CalcService) {
     $controller('getJsonData', {$scope: $scope});//继承
     $controller('constAll', {$scope: $scope});//继承
     $controller('btnSH', {$scope: $scope});//继承
     $controller('getPaper', {$scope: $scope});//继承
     $controller('disabled', {$scope: $scope});//继承
+    $scope.dataMap=new Array();
+    //多选
+    $scope.x = false; //默认未选中
+    $scope.checkSub = function() { //单选或者多选
+
+        $scope.dataMap=new Array();
+        var $input = $("#shortBoardConfrimDiv").find(".shortBoardChecked");
+        $.each($input,function(index,item){
+            console.log(index);
+            if($(item).prop("checked") == true){
+                $scope.dataMap.push($(item).attr("id"));
+                console.log($scope.dataMap);
+            }
+        })
+    };
       //默认选中函数
     function checked(data,ele,value){
         for(var i=0; i<data.length; i++){
@@ -338,7 +353,7 @@ app.controller('SBConfrimCtrl', function($scope, $controller,CalcService, $http)
     //目标分类
     $scope.target = function(type){
         $scope.formData.aimType = type;
-        $scope.query(1,1,null);
+        $scope.query(1,5,null);
     }
     $scope.query = function(page,size,callback) {
         $http.post($scope.app.testhost + '/shortSlab/teaching/getShortSlabStudentList?requestId=test123456', {
@@ -357,7 +372,119 @@ app.controller('SBConfrimCtrl', function($scope, $controller,CalcService, $http)
                 callback && callback(data.result);
             });
     }
+
+    $scope.open = function(size,dataMap) {
+        alert(dataMap);
+        if (dataMap == "" || dataMap.length == 0) { //没有选择一个的时候提示
+            alert("请至少选中一条数据在操作！");
+            return;
+        }
+        var modalShortSlabConfrimCtrlDR = $modal.open({
+            templateUrl: 'myModalContent.html',
+            controller: 'ModalShortSlabConfrimCtrlDR',
+            size: size,
+            resolve: {
+                dataMap : function(){
+                    // return $scope.diagnosticRecordsCodes;
+                    return dataMap;
+                },
+                host : function(){
+                    return $scope.app.host;
+                }
+            }
+        });
+        // modal return result
+        modalShortSlabConfrimCtrlDR.result.then(function(selectedItem) {
+            $scope.selected = selectedItem;
+        }, function() {
+            $log.info('Modal dismissed at: ' + new Date())
+        });
+    };
 });
+app.controller('ModalShortSlabConfrimCtrlDR',function($scope,$http,$controller, $modalInstance, dataMap,host) {
+    alert("jinru ")
+
+    /* $scope.selected = {
+     item: $scope.items
+     };*/
+//    $controller('getJsonData', {$scope: $scope});//继承
+    $scope.formIndex = {};
+    //  取消
+    $scope.cancel = function() {
+        $modalInstance.dismiss('cancel');
+    };
+    $scope.isShow = true;
+    $scope.showOrHide = function() {
+        $scope.isShow = !$scope.isShow;
+    };
+
+    //添加短板诊断用卷
+    $scope.addShortBoardDiag = function(){
+        $modalInstance.close($scope.selected.item);
+        var $shortBoardBox = $("#shortBoardController").find(".isChecked");
+        var $SBConfrimCtrlBox = $("#SBConfrimCtrl").find(".isChecked");
+        //获取试卷Code
+        var $aInput = $shortBoardBox.find("input:radio");
+        for(var i=0; i<$aInput.length; i++){
+            if($($aInput).eq(i).prop("checked") == true){
+                $scope.diagnosisPaperCode = $($aInput).eq(i).siblings("input:hidden").val();
+                break;
+            }
+        }
+        //获取短板分析Code
+        var $inputs = $SBConfrimCtrlBox.find("input:checked");
+        var arr = [];
+        $.each($inputs,function(index,item){
+            if($(item).prop("checked") == true){
+                if($scope.selected.item){
+                    if($(item).siblings("input").val() != $scope.selected.item.eduVulnerabilityAnalyzeRecordCode){
+                        arr.push($(item).siblings("input").val());
+                    }
+                } else {
+                    arr.push($(item).siblings("input").val());
+                }
+            }
+        });
+        if($scope.selected.item){
+            arr.push($scope.selected.item.eduVulnerabilityAnalyzeRecordCode);
+        }
+        $.each(arr,function(index,item){
+            $http.post(window.host1 + '/shortSlab/section/add/paper?requestId=test123456', {
+                "shortSlabAnalysisRecordCode":item,
+                "paperCode":$scope.diagnosisPaperCode
+            })
+                .success(function (data) {
+                    console.log(data);
+                    location.reload(true);
+                });
+        })
+    };
+//    $controller('getJsonData', {$scope: $scope});//继承
+    $scope.formData = {};
+    //默认值
+    $scope.formData.subjectCode = 1;
+    $scope.formData.bookVersionCode = "7HCcMZTzpcThi6RaByWysKQPPbtTHSj8";
+    $scope.formData.paperUseType = "p_005";
+    //筛选条件改变时执行
+    $scope.change = function(val,name){
+        $scope.formData[name] = val;
+    };
+    $scope.query = function(page,size,callback){
+        $http.post(window.testhost + '/diagnosis/list?requestId=test123456', {
+            "subjectCode":$scope.formData.subjectCode,                  //$scope.formData.subjectCode
+            "bookVersionCode":$scope.formData.bookVersionCode,       //$scope.formData.bookVersionCode
+            "paperUseType":$scope.formData.paperUseType,                //$scope.formData.paperUseType
+            "currentPage":page,
+            "pageSize":size
+        }).then(function (data) {
+            console.log(data);
+            $scope.result = data.data.result.list;
+            $scope.totalPage = data.result.totalPage;
+            callback && callback(data.result);
+        });
+    }
+});
+
 /*学生分类=》短板加课时排课*/
 app.controller('SBAddClassCtrl',function($scope, $controller, CalcService, $http, $state) {
     $controller('getJsonData', {$scope: $scope});//继承
